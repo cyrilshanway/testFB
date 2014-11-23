31e93cf6106fc7510eb81278c324e68cce7b935f
cyrilshanway <cyrilshanway@gmail.com>
2014-11-23 17:18:59 +0800

    Initial Commit

---
+++ testFB/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  testFB
+//
+//  Created by Cyrilshanway on 2014/11/23.
+//  Copyright (c) 2014年 Cyrilshanway. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ testFB/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  testFB
+//
+//  Created by Cyrilshanway on 2014/11/23.
+//  Copyright (c) 2014年 Cyrilshanway. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
